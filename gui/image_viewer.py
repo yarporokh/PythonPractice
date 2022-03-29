3a22ac6716ac8483785363de020e38d65cc6d052
yarporokh <yaroslavporohnenko@gmail.com>
2022-03-29 15:31:30 +0300

    Added menu bar

--- gui/image_viewer.py
+++ gui/image_viewer.py
@@ -4,4 +4,14 @@ from tkinter import ttk
 root = tk.Tk()
 root.title("Image Viewer")
 
+menubar = tk.Menu()
+root.config(menu=menubar)
+root.option_add("*tearOff", False)
+
+file_menu = tk.Menu(menubar)
+
+menubar.add_cascade(label="Open", menu=file_menu)
+file_menu.add_command(label="Open dir")
+file_menu.add_command(label="Open image")
+
 root.mainloop()
