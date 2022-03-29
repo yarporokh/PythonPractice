f6b1739da6d88a25f5ad176681b560adad95c391
yarporokh <yaroslavporohnenko@gmail.com>
2022-03-29 18:02:56 +0300

    Add open image functionality

--- gui/image_viewer.py
+++ gui/image_viewer.py
@@ -1,9 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+
+def open_image():
+    global label
+    f_types = [('PNG/JPG Files', '*.png; *jpg')]
+    file = tk.filedialog.askopenfilename(filetypes=f_types)
+    img = Image.open(file)
+    img = img.resize((500, 500))
+    tkimage = ImageTk.PhotoImage(img)
+    label = tk.Label(root, image=tkimage)
+    label.image =tkimage
+    label.grid(row=1, column=0, columnspan=3)
 
 root = tk.Tk()
 root.title("Image Viewer")
-root.geometry("500x500")
+root.geometry("500x550")
+root.resizable(False, False)
 
 menubar = tk.Menu()
 root.config(menu=menubar)
@@ -13,20 +28,17 @@ file_menu = tk.Menu(menubar)
 
 menubar.add_cascade(label="Open", menu=file_menu)
 file_menu.add_command(label="Open dir")
-file_menu.add_command(label="Open image")
+file_menu.add_command(label="Open image", command=open_image)
 
-img_frame = tk.Frame(root)
-img_frame.pack(side="top")
-btn_frame = tk.Frame(root)
-btn_frame.pack(side="bottom")
 
-back_button = ttk.Button(btn_frame, text="Back")
+label = tk.Label(text="No Image")
+label.grid(row=1, column=0, columnspan=3)
 
-quit_button = ttk.Button(btn_frame, text="Quit", command=root.destroy)
+back_button = ttk.Button(root, text="Back", state=tk.DISABLED)
+quit_button = ttk.Button(root, text="Quit", command=root.destroy)
+next_button = ttk.Button(root, text="Next", state=tk.DISABLED)
 
-next_button = ttk.Button(btn_frame, text="Next")
-
-back_button.grid(row=0, column=1)
-next_button.grid(row=0, column=2)
-quit_button.grid(row=0, column=3)
+back_button.grid(row=5, column=0)
+next_button.grid(row=5, column=1)
+quit_button.grid(row=5, column=2)
 root.mainloop()
