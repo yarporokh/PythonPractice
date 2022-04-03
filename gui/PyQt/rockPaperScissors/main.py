0cfecb7fe01a70bdb222cee781dd2498c25c2a2e
yarporokh <yaroslavporohnenko@gmail.com>
2022-04-03 13:32:40 +0300

    Added selection functions

--- gui/PyQt/rockPaperScissors/main.py
+++ gui/PyQt/rockPaperScissors/main.py
@@ -1,3 +1,5 @@
+import random
+
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 
@@ -41,32 +43,70 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.player_score = 0
+        self.computer_score = 0
+        self.CHOICES = ["rock", "paper", "scissors"]
 
         self.functions()
-
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Your choice:"
-                                                    "</span></p>"
-                                                    "<p><span style=\" font-size:18pt;\">Computer choice: </span></p></body></html>"))
-        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Your score:"
-                                                      "</span></p>"
-                                                      "<p><span style=\" font-size:18pt;\">Computer score: "
-                                                      "</span></p></body></html>"))
+        self.label.setText(
+            _translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Your choice:"
+                                     "</span></p>"
+                                     "<p><span style=\" font-size:18pt;\">Computer choice: </span></p></body></html>"))
+        self.label_2.setText(
+            _translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Your score:"
+                                     "</span></p>"
+                                     "<p><span style=\" font-size:18pt;\">Computer score: "
+                                     "</span></p></body></html>"))
         self.rockB.setText(_translate("MainWindow", "Rock"))
         self.paperB.setText(_translate("MainWindow", "Paper"))
         self.scissorsB.setText(_translate("MainWindow", "Scissors"))
 
     def functions(self):
-        self.rockB.clicked.connect()
-        self.paperB.clicked.connect()
-        self.scissorsB.clicked.connect()
+        self.rockB.clicked.connect(lambda: self.choose_rock())
+        self.paperB.clicked.connect(lambda: self.choose_paper())
+        self.scissorsB.clicked.connect(lambda: self.choose_scissors())
+
+    def computer_choice(self):
+        return random.choice(self.CHOICES)
+
+    def choose_rock(self):
+        choice = self.computer_choice()
+        if choice == "rock":
+            pass
+        elif choice == "paper":
+            self.computer_score += 1
+        else:
+            self.player_score += 1
+        # update_labels("rock", choice)
+
+    def choose_paper(self):
+        choice = self.computer_choice()
+        if choice == "rock":
+            self.player_score += 1
+        elif choice == "paper":
+            pass
+        else:
+            self.computer_score += 1
+        # update_labels("paper", choice)
+
+    def choose_scissors(self):
+        choice = self.computer_choice()
+        if choice == "rock":
+            self.computer_score += 1
+        elif choice == "paper":
+            self.player_score += 1
+        else:
+            pass
+        # update_labels("scissors", choice)
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
